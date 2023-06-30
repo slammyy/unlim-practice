bd94e4678a31ca01c466d8a329cf5a0b96a064db
s1ammy <maksimsavincev@gmail.com>
2023-06-30 17:55:21 +0300

    chore: change order of services

--- task/screens/Tasks.js
+++ task/screens/Tasks.js
@@ -1,34 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Service from '../companents/Service';
 
 const Tasks = () => {
     return (
-        <View>
+        <ScrollView>
             <StatusBar />
             <View style={styles.container}>
-                <Service text="Трансферы AirBack" icon="plane" />
-                <Service text="Бизнес залы" icon="suitcase" />
-                <Service text="Юрист" icon="legal" />
+                <Service text="Дэшборд бизнеса" icon="pie-chart" />
+                <Service text="Продажи" icon="line-chart" />
+                <Service text="Маркетинг" icon="shopping-cart" />
                 <Service text="Бухгалтер" icon="money" />
+                <Service text="Юрист" icon="legal" />
+                <Service text="Логистика" icon="truck" />
                 <Service text="Финансовые услуги" icon="credit-card" />
                 <Service text="Контакты клуба" icon="id-card-o" />
                 <Service text="Инвестиции" icon="bar-chart" />
                 <Service text="Эдвайзеры" icon="group" />
-                <Service text="Логистика" icon="truck" />
-                <Service text="Продажи" icon="line-chart" />
-                <Service text="Дэшборд" icon="pie-chart" />
-                <Service text="Маркетинг" icon="shopping-cart" />
+                <Service text="Бизнес мероприятия" icon="calendar" />
+                <Service text="Трансферы AirBack" icon="plane" />
+                <Service text="Бизнес залы" icon="suitcase" />
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         marginVertical: 30,
+        marginHorizontal: 20,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         flexDirection: 'row',
         flexWrap: 'wrap',
         gap: 25,
