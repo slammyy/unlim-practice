298a7a77b58f1ff2e4b174dffeaeeee9e169145e
s1ammy <maksimsavincev@gmail.com>
2023-07-06 11:05:14 +0300

    chore: change border color

--- task/companents/Theme.js
+++ task/companents/Theme.js
@@ -4,7 +4,7 @@ const Theme = {
         primary: '#0961d9',
         background: '#f2f2f2',
         card: '#fff',
-        border: '#fff',
+        border: '#dbdbdb',
         text: '#000',
     },
 };
